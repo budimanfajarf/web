f90a9b606fce870f204538fe809486777031b048
budimanfajarf <budimanfajarf@gmail.com>
2020-05-06 09:08:38 +0700

    off translate, update index, update favicon

--- assets/js/my-typeit.js
+++ assets/js/my-typeit.js
@@ -1,16 +1,20 @@
 const myTypeit = new TypeIt("#home-subtitle", {
   speed: 140,
   startDelay: 2000,  
-  waitUntilVisible: true,  
+  waitUntilVisible: true,
 })
 
 myTypeit.type("Hello,")
         .pause(700)
         .type(" I'm Budi ☕")
         .pause(1800)
-        .type(" Backend Developer💻")
+        .type(" Backend Developer 💻")
+        .pause(1100)
+        .break()
         .pause(900)
-        .type(" from Bandung,")
+        .type("📌")
+        .pause(900)
+        .type(" Bandung,")
         .pause(300)
         .type(" Indonesia")
         .go();
