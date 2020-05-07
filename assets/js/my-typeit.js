75a6b1cb7ae509da335046a30c06b53bef29c2f8
budimanfajarf <budimanfajarf@gmail.com>
2020-05-07 16:40:46 +0700

    change scriptpt & style to relURL, update meta, update typeit seo

--- assets/js/my-typeit.js
+++ assets/js/my-typeit.js
@@ -1,20 +1,23 @@
 const myTypeit = new TypeIt(".home-subtitle-typeit", {
-  speed: 140,
+  speed: 135,
   startDelay: 2000,  
   waitUntilVisible: true,
 })
 
-myTypeit.type("Hello,")
-        .pause(700)
-        .type(" I'm Budi ☕")
-        .pause(1800)
-        .type(" Backend Developer 💻")
-        .pause(1100)
-        .break()
-        .pause(900)
-        .type("📌")
-        .pause(900)
-        .type(" Bandung,")
-        .pause(300)
-        .type(" Indonesia")
-        .go();
+myTypeit.go();
+
+// myTypeit.type("Hello,")
+//         .pause(700)
+//         .type(" I'm Budi ☕")
+//         .pause(1800)
+//         .type(" Backend Developer 💻")
+//         .pause(1100)
+//         .break()
+//         .pause(900)
+//         .type("📌")
+//         .pause(900)
+//         .type(" Bandung,")
+//         .pause(300)
+//         .type(" Indonesia")
+//         .go();
+
