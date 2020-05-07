4d341eb4d0694b187fcc3046f0767be439b4f034
budimanfajarf <budimanfajarf@gmail.com>
2020-05-08 06:27:55 +0700

    add light theme

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+// check localStorage
+const preferredTheme = localStorage.getItem('preferredTheme');
+
+if (preferredTheme == 'light')
+  setLightMode(true);
+else
+  setLightMode(false);
+
+function setLightMode(isLight) {
+  const html = document.documentElement;
+  const lightBtn = document.getElementById('light-btn');
+  const darkBtn = document.getElementById('dark-btn');
+
+  if(isLight) {
+    localStorage.setItem('preferredTheme', 'light');
+    html.classList.add('light');    
+    lightBtn ? lightBtn.style.display = "none" : '';
+    darkBtn ? darkBtn.style.display = "inline-block" : '';    
+  } else {
+    localStorage.removeItem('preferredTheme');
+    html.classList.remove('light');
+    darkBtn ? darkBtn.style.display = "none" : '';
+    lightBtn ? lightBtn.style.display = "inline-block" : '';
+  }
+}
