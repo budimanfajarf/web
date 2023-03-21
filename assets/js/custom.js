f3d7fd878a96d4037de33c6f0e0555aee1a11974
Budi <budimanfajarf@gmail.com>
2023-03-21 20:03:45 +0700

    Remove switch dark / light mode capability

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,25 +1,25 @@
 // check localStorage
 const preferredTheme = localStorage.getItem('preferredTheme');
 
-if (preferredTheme == 'light')
-  setLightMode(true);
-else
-  setLightMode(false);
+if (preferredTheme == 'light') setLightMode(true);
+else setLightMode(false);
 
 function setLightMode(isLight) {
+  isLight = false; // force dark mode
+
   const html = document.documentElement;
   const lightBtn = document.getElementById('light-btn');
   const darkBtn = document.getElementById('dark-btn');
 
-  if(isLight) {
+  if (isLight) {
     localStorage.setItem('preferredTheme', 'light');
     html.classList.add('light');
-    lightBtn ? lightBtn.style.display = "none" : '';
-    darkBtn ? darkBtn.style.display = "inline-block" : '';
+    lightBtn ? (lightBtn.style.display = 'none') : '';
+    darkBtn ? (darkBtn.style.display = 'inline-block') : '';
   } else {
     localStorage.removeItem('preferredTheme');
     html.classList.remove('light');
-    darkBtn ? darkBtn.style.display = "none" : '';
-    lightBtn ? lightBtn.style.display = "inline-block" : '';
+    darkBtn ? (darkBtn.style.display = 'none') : '';
+    lightBtn ? (lightBtn.style.display = 'inline-block') : '';
   }
 }
